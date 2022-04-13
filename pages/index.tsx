a46f43a5b63fcb0978df72b99bec509706f82239
kamesuke42 <kamesuke42@kamesuke42.com>
2022-04-14 00:22:54 +0900

    [nextjs] Add Prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import type {NextPage} from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
@@ -10,11 +10,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
